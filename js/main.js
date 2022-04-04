75905eeee8aab8cdc32a6ad68ddb33d583f1779c
Michael Suh <michaelsuh9901@gmail.com>
2022-04-04 23:37:13 +0000

    in progress

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+var $photoURL = document.querySelector('.photourl');
+var $preview = document.querySelector('.preview');
+
+function updateImg(event) {
+  $preview.src = $photoURL.value;
+}
+
+$photoURL.addEventListener('input', updateImg);
