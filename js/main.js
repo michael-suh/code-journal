b019074bc1330d1627e022eb74b7a89dd65b4f2b
Michael Suh <michaelsuh9901@gmail.com>
2022-04-05 03:48:49 +0000

    issue-1-create-an-entry - Michael Suh

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ var $photoURL = document.querySelector('.photourl');
 var $preview = document.querySelector('.preview');
 
 function updateImg(event) {
-  $preview.src = $photoURL.value;
+  $preview.setAttribute('src', $photoURL.value);
 }
 
 $photoURL.addEventListener('input', updateImg);
+
+var $form = document.querySelector('form');
+
+function updateEntry(event) {
+  event.preventDefault();
+  var entriesObj = {};
+  data.nextEntryId++;
+  data.entries.unshift(entriesObj);
+  $preview.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
+
+$form.addEventListener('submit', updateEntry);
